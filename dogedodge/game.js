84227c84df67715d766024965659191ef2297711
FrontSide <drieger@gmx.at>
2014-03-30 19:29:57 +0200

    add coins

--- dogedodge/game.js
+++ dogedodge/game.js
@@ -22,14 +22,14 @@ function keyListener() {
   window.onkeydown = function(e) {
      var key = e.keyCode ? e.keyCode : e.which;
 
-     // Cursor Right -> 37
-     // Cursor Left  -> 39
+     // Cursor Right -> 39
+     // Cursor Left  -> 37
 
      //Activate Movement if key is pressed
-     if (key == 37) {
+     if (key == 39) {
          moveLeft = false;
          moveRight = true;
-     }else if (key == 39) {
+     }else if (key == 37) {
          moveLeft = true;
          moveRight = false;
      }
@@ -39,13 +39,13 @@ function keyListener() {
   window.onkeyup = function(e) {
      var key = e.keyCode ? e.keyCode : e.which;
 
-     // Cursor Right -> 37
-     // Cursor Left  -> 39
+     // Cursor Right -> 39
+     // Cursor Left  -> 37
 
      //Activate Movement if key is pressed
-     if (key == 37) {
+     if (key == 39) {
          moveRight = false;
-     }else if (key == 39) {
+     }else if (key == 37) {
          moveLeft = false;
      }
   }
@@ -54,21 +54,24 @@ function keyListener() {
 
 /* Image Paths */
 dogefig_src = "img/dogefig.jpeg";
+dogcoin_src = "img/dogecoin.png";
+bitcoin_src = "img/bitcoin.png";
 
 /* Dodge Fox */
 dodgefigX = Math.ceil(canvas.width/2)-42;
 moveRight = false;
 moveLeft = false;
+
 /* Draw Dodge Fox */
 function drawDogeFig() {
 
   //Check X Movement
   if (moveRight) {
-    dodgefigX += 10;
+    dodgefigX += 15;
   }
 
   else if (moveLeft) {
-    dodgefigX -= 10;
+    dodgefigX -= 15;
   }
 
   dogefig = new Image();
@@ -79,14 +82,63 @@ function drawDogeFig() {
 
 }
 
+/* Coins */
+coins = new Array();
+function o_coin(isDoge, xPos, yPos, ySpeed) {
+  this.isDoge = isDoge;
+  this.xPos = xPos;
+  this.yPos = yPos;
+  this.ySpeed = ySpeed;
+}
+
+/* Draw Coin */
+function drawCoin() {
+
+  for (i=0; i<coins.length; i++) {
+
+    it_coin = coins[i];
+    coin = new Image();
+    if (it_coin.isDoge) {
+      coin.src = dogcoin_src;
+    } else {
+      coin.src = bitcoin_src;
+    }
+
+    coin.onload = function() {
+        context.drawImage(coin, it_coin.xPos, it_coin.yPos);
+    }
+
+    // Move Coin
+    it_coin.yPos += 10;
+
+  }
+
+}
+
 /* Draw Loop - Main */
-function mainDraw() {
+function mainLoop() {
 
   //Clear Canvas
   canvas.width = canvas.width;
 
   //Draw Fox
   drawDogeFig();
+
+  /*Generate Coin
+  var coinSeed = Math.ceil(Math.random()*100);
+  if (coinSeed % 3 == 0) {
+    var xPos = Math.ceil(Math.random()*(canvas.width-100))
+    if (coinSeed % 2 == 0) {
+      coins.push(new Coin(true, 200, 0, 1.2))
+    } else {
+      coins.push(new Coin(false, 200, 0, 1.2))
+    }
+  } */
+
+  coins.push(new o_coin(true, 200, 0, 2));
+
+  //Draw Coins
+  drawCoin();
 
 }
 
@@ -96,7 +148,7 @@ function mainDraw() {
 ** which is needed in a standard setInterval(). That means we need to
 ** create a function in setInterval() which then calls the function we actually need
 ***/
-setInterval(function(){mainDraw();},50);
+setInterval(function(){mainLoop();},100);
 keyListener();
 
 
