03629816d362cc2cb28b7e79a1082b6d3fdd1ca3
FrontSide <drieger@gmx.at>
2014-03-31 20:41:39 +0200

    change size og window

--- dogedodge/appbg.js
+++ dogedodge/appbg.js
@@ -7,7 +7,7 @@ chrome.app.runtime.onLaunched.addListener(function() {
 
   chrome.app.window.create('window.html', {
     'bounds':{
-      'width':500,
+      'width':1000,
       'height':500
     }
   });
