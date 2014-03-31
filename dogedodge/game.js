bf10ce7924bf68e22a27b7925f06517d4a8ee4d2
FrontSide <drieger@gmx.at>
2014-03-31 15:42:00 +0200

    first playable

--- dogedodge/game.js
+++ dogedodge/game.js
@@ -52,10 +52,24 @@ function keyListener() {
 
 }
 
+/* Played Seconds */
+startsec = Math.floor((new Date().getTime())/1000);
+function getTime() {
+  return Math.floor((new Date().getTime())/1000) - startsec;
+}
+
 /* Image Paths */
 dogefig_src = "img/dogefig.jpeg";
 dogcoin_src = "img/dogecoin.png";
 bitcoin_src = "img/bitcoin.png";
+
+/* Points and Life */
+lives = 10;
+points = 0;
+
+/* Coin Generator modulo constants CGMC */
+mainCGMC = 50;
+dogeCGMC = 100;
 
 /* Dodge Fox */
 dodgefigX = Math.ceil(canvas.width/2)-42;
@@ -66,12 +80,17 @@ moveLeft = false;
 function drawDogeFig() {
 
   //Check X Movement
-  if (moveRight) {
+  if (moveRight && (dodgefigX < (canvas.width-85))) {
     dodgefigX += 15;
   }
 
-  else if (moveLeft) {
+  else if (moveLeft && (dodgefigX > 0)) {
     dodgefigX -= 15;
+  }
+
+  //If fox is out of the window (right) in cas window was resized
+  if (dodgefigX > (canvas.width-85)) {
+    dodgefigX = canvas.width-85;
   }
 
   dogefig = new Image();
@@ -84,7 +103,9 @@ function drawDogeFig() {
 
 /* Coins */
 coins = new Array();
-function o_coin(isDoge, xPos, yPos, ySpeed) {
+function o_coin(img, isDoge, xPos, yPos, ySpeed) {
+  this.isActive = true; //No more active when collission
+  this.img = img;
   this.isDoge = isDoge;
   this.xPos = xPos;
   this.yPos = yPos;
@@ -94,60 +115,108 @@ function o_coin(isDoge, xPos, yPos, ySpeed) {
 /* Draw Coin */
 function drawCoin() {
 
-  for (var i=0; i<coins.length; i++) {
+  for (var i = 0; i<coins.length; i++) {
 
     it_coin = coins[i];
 
-    console.log("coin nr: " + i);
-    console.log("coin yPos: " + it_coin.yPos);
-
-    coin = new Image();
-    if (it_coin.isDoge) {
-      coin.src = dogcoin_src;
-    } else {
-      coin.src = bitcoin_src;
+    if (it_coin.isActive) {
+      context.drawImage(it_coin.img, it_coin.xPos, it_coin.yPos);
+      detectCol(it_coin);
     }
 
-    coin.onload = function() {
-        context.drawImage(coin, it_coin.xPos, it_coin.yPos);
+    if(it_coin.yPos >= canvas.height) {
+	      coins.shift();
     }
 
-    // Move Coin
-    coins[i].yPos += 10;
+    else {
+      // Move Coin
+      coins[i].yPos += 5;
+    }
+
 
   }
+
+}
+
+/* Collisson detection */
+function detectCol(col_coin) {
+
+  var x = col_coin.xPos;
+  var y = col_coin.yPos;
+  //Height and width is actually 50
+  //but I'm gonna use some clearance
+  var h = 40;
+  var w = 40;
+
+  var isDoge = col_coin.isDoge;
+  var col = false;
+
+  if (((x+w) >= dodgefigX) && (x <= (dodgefigX+85)) && ((y+h) >= 200) && (y < 285)) {
+    col = true;
+  }
+
+  if (col) {
+    col_coin.isActive = false;
+    if (isDoge) {
+      points += 100;
+    }
+
+    else {
+      lives -= 1;
+    }
+  }
+
+}
+
+/* Draw Stats */
+function drawStats() {
+
+  context.fillStyle = "#2222FF";
+  context.font = "bold 30px sans-serif";
+  context.fillText(points, canvas.width-100, canvas.height-100);
+  context.fillText(lives, canvas.width-100, canvas.height-150);
+  context.fillText(getTime(), canvas.width-100, canvas.height-200);
 
 }
 
 /* Draw Loop - Main */
 function mainLoop() {
 
-  //Clear Canvas
-  canvas.width = canvas.width;
+  //Clear Canvas + Window is now resizable
+  canvas.width = window.innerWidth;
 
   //Draw Fox
   drawDogeFig();
 
-  //Generate Coin
-  var coinSeed = Math.ceil(Math.random()*100);
-  if (coinSeed % 3 == 0) {
-    var xPos = Math.ceil(Math.random()*(canvas.width-100))
-    if (coinSeed % 2 == 0) {
-      coins.push(new o_coin(true, xPos, 0, 1.2))
-    } else {
-      coins.push(new o_coin(false, xPos, 0, 1.2))
-    }
-  }
-
-
   //Draw Coins
   drawCoin();
 
-  console.log("coins array size:" + coins.length);
+  //DrawPoints and Lives
+  drawStats();
+
+  //Generate Coin
+  var coinSeed = Math.ceil(Math.random()*500);
+  console.log("coins seed: " + coinSeed);
+
+  if (coinSeed % mainCGMC == 0) {
+
+    var img = new Image();
+    var xPos = Math.ceil(Math.random()*(canvas.width-100))
+
+    if (coinSeed % dogeCGMC == 0) {
+      img.src = dogcoin_src;
+      coins.push(new o_coin(img, true, xPos, 0, 1.2))
+    }
+
+    else {
+      img.src = bitcoin_src;
+      coins.push(new o_coin(img, false, xPos, 0, 1.2))
+    }
+
+    console.log("coins array size:" + coins.length);
+  }
 
 }
-
-
 
 /*** Chrome doesn't support converting a String into code
 ** which is needed in a standard setInterval(). That means we need to
